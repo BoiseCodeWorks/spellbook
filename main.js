85e766813bf63511b1908aaa71c6aa2b4e3deb28
Jake Overall <joverall22@gmail.com>
2018-08-15 10:31:28 -0600

    Yay Isaiah White

---
+++ main.js
@@ -0,0 +1,10 @@
+import SpellbookController from "./app/components/spellbook-controller.js"
+
+class App {
+  constructor() {
+    this.controllers = {
+      spellbook: new SpellbookController
+    }
+  }
+}
+window.app = new App
